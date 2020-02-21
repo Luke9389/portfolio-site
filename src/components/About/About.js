ec22abaac50435b825c9e45a725099db2c227deb
Luke Donahue <luke9389@gmail.com>
2020-02-21 10:33:30 -0800

    updated about me blurb

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,7 +6,7 @@ const About = () => {
     <section className={styles.About}>
       <div className={styles.AboutMeWrapper}>
         <h1>About Me</h1>
-        <p>I am a software developer based in Portland OR. I love learning new tools and applying them to creative work. Here to defend the internet from devs who think DRY = bland.</p>
+        <p>I am a synth patch designer, board game optimizer, armchair philosopher, cat snuggler, beer taster, and software developer based in Portland OR. I love learning new tools and applying them to creative work. Here to defend the internet from devs who think DRY = bland.</p>
       </div>
     </section>
   );
